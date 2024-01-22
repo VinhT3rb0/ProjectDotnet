241b0fb6c7fb0f09a3c9a2592ced3f9f17469bb8
VinhT3rB0 <vinhvinh132465@gmail.com>
2024-01-22 20:16:47 +0700

    stt

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -1,20 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace WpfApp2
 {
-    public partial class MovieView : Window
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-        List<Movie> movies = new List<Movie>();
-
-        public MovieView()
+        public MainWindow()
         {
             InitializeComponent();
-            movies.Add(new Movie { TenPhim = "Avengers: Cuộc Chiến Vô Cực", GioChieu = "18:00", TinhTrang = "65/100", GiaVe = 100000, DanhGia = 4 });
-            movieSelectionGrid.ItemsSource = movies;
-            movies.Add(new Movie { TenPhim = "Avengers: Cuộc Chiến Vô Cực", GioChieu = "19:00", TinhTrang = "27/100", GiaVe = 100000, DanhGia = 4 });
-            movieSelectionGrid.ItemsSource = movies;
+
         }
     }
 }
---
+++ WpfApp2/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    class Movie
+    {
+        public string TenPhim { get; set; }
+        public string GioChieu { get; set; }
+        public string TinhTrang { get; set; }
+        public int GiaVe { get; set; }
+        public int DanhGia { get; set; }
+    }
+}
